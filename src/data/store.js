af05c0423202d34fb04242dfd5b27b7e650fb882
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-05-30 18:38:08 +0200

    fix headerlink in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    headerLink: [
+    headerLink: {
         home:{
             title: "HOME",
             href: "#"
@@ -26,5 +26,5 @@ export const store = reactive({
             title: "ELEMENTS",
             href: "#"
         }
-    ]
+    }
 })
